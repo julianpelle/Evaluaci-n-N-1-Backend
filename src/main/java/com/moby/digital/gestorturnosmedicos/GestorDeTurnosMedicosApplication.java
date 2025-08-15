bbb78fb8fdf90690ec3a893c87490532f01448e9
Julian Mauro Pellegrini <pellegrinijulianmauro@gmail.com>
2025-08-15 20:35:09 -0300

    Creacion del proyecto

---
+++ src/main/java/com/moby/digital/gestorturnosmedicos/GestorDeTurnosMedicosApplication.java
@@ -0,0 +1,13 @@
+package com.moby.digital.gestorturnosmedicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeTurnosMedicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeTurnosMedicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moby/digital/gestorturnosmedicos/GestorDeTurnosMedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moby.digital.gestorturnosmedicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeTurnosMedicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
