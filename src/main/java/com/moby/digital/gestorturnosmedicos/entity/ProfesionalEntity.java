d170ccf10f83c42083a14fe77d71fee2c7e915ed
Julian Mauro Pellegrini <pellegrinijulianmauro@gmail.com>
2025-08-21 01:18:56 -0300

    Entidad de profesionales creada

---
+++ src/main/java/com/moby/digital/gestorturnosmedicos/entity/ProfesionalEntity.java
@@ -0,0 +1,30 @@
+package com.moby.digital.gestorturnosmedicos.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "profesionales")
+@Getter
+@Setter
+public class ProfesionalEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull(message = "El id no puede ser nulo")
+    private Long id;
+
+    @NotBlank(message = "El Nombre no puede estar vacío")
+    private String nombre;
+
+    @NotBlank(message = "El apellido no puede estar vacío")
+    private String apellido;
+
+    @NotBlank(message = "La especialidad no puede estar vacía")
+    private String especialidad;
+
+    public ProfesionalEntity() {
+    }
+}
---
+++ src/main/java/com/moby/digital/gestorturnosmedicos/repository/ProfesionalRepository.java
@@ -0,0 +1,8 @@
+package com.moby.digital.gestorturnosmedicos.repository;
+
+import com.moby.digital.gestorturnosmedicos.entity.ProfesionalEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfesionalRepository extends JpaRepository<ProfesionalEntity, Long> {
+
+}
