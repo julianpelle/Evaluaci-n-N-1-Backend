715e9ad85e4ae53e205ec8d5b6da1c0258c745d3
Julian Mauro Pellegrini <pellegrinijulianmauro@gmail.com>
2025-08-21 02:29:03 -0300

    Handler creado

--- src/main/java/com/moby/digital/gestorturnosmedicos/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/moby/digital/gestorturnosmedicos/exception/handler/GlobalExceptionHandler.java
@@ -1,4 +1,16 @@
 package com.moby.digital.gestorturnosmedicos.exception.handler;
 
+import com.moby.digital.gestorturnosmedicos.exception.RecursoNoEncontradoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RecursoNoEncontradoException.class)
+    public ResponseEntity<String> handleNoEncontrado(RecursoNoEncontradoException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 }
