e3d938bbe9da264136601a122f96cd298b8f7e81
Julian Mauro Pellegrini <pellegrinijulianmauro@gmail.com>
2025-08-21 18:01:16 -0300

    Manejo de error 500 con clase de testeo para forzar el mismo

---
+++ src/main/java/com/moby/digital/gestorturnosmedicos/controller/testController.java
@@ -0,0 +1,15 @@
+package com.moby.digital.gestorturnosmedicos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+
+    @GetMapping
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
