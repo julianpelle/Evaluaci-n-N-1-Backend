ed48eeff52682f3228e1a70cc1f7c856cdd77470
Julian Mauro Pellegrini <pellegrinijulianmauro@gmail.com>
2025-08-21 02:11:47 -0300

    Turno service creado sin validaciones

--- src/main/java/com/moby/digital/gestorturnosmedicos/repository/TurnoRepository.java
+++ src/main/java/com/moby/digital/gestorturnosmedicos/repository/TurnoRepository.java
@@ -3,5 +3,11 @@ package com.moby.digital.gestorturnosmedicos.repository;
 import com.moby.digital.gestorturnosmedicos.entity.TurnoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface TurnoRepository extends JpaRepository<TurnoEntity, Long> {
+
+    List<TurnoEntity> findByDate(LocalDate date);
+
 }
